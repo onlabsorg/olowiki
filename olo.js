32357adb8527e00ac16bb67e9db117858f51231b
Marcello Del Buono <m.delbuono@gmail.com>
2018-05-21 08:16:53 +0200

    first release

--- olo.js
+++ olo.js
@@ -1,64 +1,111 @@
-const portParamIndex = process.argv.indexOf("--port") + 1;
-const port = portParamIndex ? process.argv[portParamIndex] : 8010;
+
+const port = 8080;
+const rootPath = __dirname;
+const storePath = "/store";
 
 const fs = require("fs");
-const path = require("path");
 
-const basePath = `${__dirname}/client`;
-const dataPath = path.normalize(`${__dirname}/../.olo`);
+const jwtKey = require("./private/jwt-key");
 
+const passport = require('passport');
+const passportGoogleOpenIdConnect = require( 'passport-google-openidconnect' );
+const googleClientId = JSON.parse(fs.readFileSync(`${rootPath}/private/google_client_id.json`, {encoding:'utf8'}));
 
+const OloServer = require("./src/server");
+const Store = require("./src/server/fs-store");
+const store = new Store(`${rootPath}/store`, {jwtKey});
 
-// Initialize olojs Router
-
-const Auth = require("./client/lib/olojs/auth");
-const {FileStore, Router} = require("./client/lib/olojs/store-server");
-const olojsStore = new FileStore(`${dataPath}/store`);
-const routerOptions = JSON.parse(fs.readFileSync(`${dataPath}/config.json`));
-const olojsRouter = new Router(olojsStore, routerOptions);
-
-
-
-// Create express app
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use("*", function (req, res, next) {
-    req.userName = req.get('Authorization') || 'guest';
-    next();
+
+
+// prevent client access to private folder
+app.all("/private/*", (req, res, next) => {
+    res.status(403).send();
 });
 
-app.get("/", function (req, res, next) {
-    const indexDoc = fs.readFileSync(`${basePath}/index.html`);
-    res.set('Content-Type', "text/html");
-    res.status(200).send(indexDoc);
+
+
+// parse the json request body
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+
+
+// add olo services
+app.use( OloServer(store, storePath) );
+
+
+
+
+// handle google openidconnect signin
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
-app.get("/docs/*", function (req, res, next) {
-    if (!req.xhr) {
-        const indexDoc = fs.readFileSync(`${basePath}/index.html`);
-        res.set('Content-Type', "text/html");
-        res.status(200).send(indexDoc);
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+passport.use( (function googleOpenIdConnectStrategy () {
+
+    const options = {
+        clientID: googleClientId.web.client_id,
+        clientSecret: googleClientId.web.client_secret,
+        callbackURL: "http://localhost:8080/auth/google/callback"
+    };
+    
+    const callback = (iss, sub, profile, accessToken, refreshToken, done) => {
+        process.nextTick(function () {   
+            return done(null, {
+                name: profile.displayName,
+                id: profile._json.email,
+                googleAccessToken: accessToken
+            });
+        });
     }
-    else {
-        next();
-    }
+
+    return new passportGoogleOpenIdConnect.Strategy(options, callback);
+    
+})() );
+
+
+
+
+app.use( passport.initialize() );
+
+app.get('/auth/google', passport.authenticate('google-openidconnect', {scope: ['email', 'profile']}));
+
+app.get('/auth/google/callback', [
+    
+    passport.authenticate('google-openidconnect', {
+        failureRedirect: '/login', 
+        session: false
+    }),
+
+    (req, res) => {
+        const token = store.generateToken(req.user);                
+        res.redirect(`/src/server/callback.html?user=${token}`);
+    }    
+]);
+         
+
+app.post('/auth/google/revoke', (req, res, next) => {
+    const strategy = req._passport.instance._strategy('google-openidconnect');
+    strategy.revoke( { accessToken: req.olo.user.googleAccessToken }, 
+            (err, body) => res.status(200).send());
 });
 
-app.use('/docs', olojsRouter);
-
-app.use(express.static(basePath, {etag:false}));
 
 
+// serve static files
+app.use(express.static(rootPath));
 
-// Create and start http server
 
-const http = require('http');
-const server = http.createServer(app);
 
-server.listen(port, function () {
-    console.log(`Test HTTP server listening on port ${port}!`);
-    console.log(`Root token: ${routerOptions.rootToken}`);
-    console.log();
+// start listening for HTTP requests
+app.listen(port, () => {
+    console.log(`olo server listening on port ${port} ...`);
 });
