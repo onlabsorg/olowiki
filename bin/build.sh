dff760ccbe1ba33d981b94e24cc0566d50509877
Marcello Del Buono <m.delbuono@gmail.com>
2018-06-22 23:03:03 +0200

    none

--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+
+echo
 echo ">>> WEBPACK"
 #webpack --mode production --display-modules
 
+echo
 echo ">>> COMMIT"
 git add --all 
 git commit -a -m $2
 
+echo
 echo ">>> INCREASE VERSION"
 npm version $1
 git commit -a -m "Packed!" 
 
-echo ">>> DONE"
+echo
