8714ed16cf9cfdcbe6953ad3e8696702447ee8d9
Marcello Del Buono <m.delbuono@gmail.com>
2018-06-23 23:34:07 +0200

    testing stuff

--- bin/build.sh
+++ bin/build.sh
@@ -2,12 +2,15 @@
 
 
 echo
+echo ">>> INCREASE VERSION"
+npm version $1
+
+echo
 echo ">>> WEBPACK"
 webpack --mode production --display-modules
 
 echo
-echo ">>> INCREASE VERSION AND COMMIT"
-npm version $1
+echo ">>> COMMIT"
 git commit -a -m "Packed!" 
 
 echo
