bf8931f0c77530434ad375b0438a538217086de9
Marcello Del Buono <m.delbuono@gmail.com>
2018-08-30 20:47:04 +0200

    home page route is now a parameter in config.json

--- dist/olo-editor.bundle.js
+++ dist/olo-editor.bundle.js
@@ -1,1 +1,25 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{260:function(t,n,e){t.exports={template:e(268),components:{"ace-editor":e(267)},props:["value","lang"],data:()=>Object({editorOptions:{fontSize:"12pt"}}),computed:{content:{get(){return this.value},set(t){this.$emit("input",t)}}},methods:{initEditor(){e(264),e(263),e(262)}}}},268:function(t,n){t.exports='<ace-editor class="olo-editor"\n            v-model="content"\n            @init="initEditor"\n            :lang="lang"\n            theme="chrome"\n            :options="editorOptions">\n        </ace-editor>\n'}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["olo-editor"],{
+
+/***/ "./lib/olo-webui/olo-editor.html":
+/*!***************************************!*\
+  !*** ./lib/olo-webui/olo-editor.html ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = \"<ace-editor class=\\\"olo-editor\\\"\\n            v-model=\\\"content\\\"\\n            @init=\\\"initEditor\\\"\\n            :lang=\\\"lang\\\"\\n            theme=\\\"chrome\\\"\\n            :options=\\\"editorOptions\\\">\\n        </ace-editor>\\n\";\n\n//# sourceURL=webpack:///./lib/olo-webui/olo-editor.html?");
+
+/***/ }),
+
+/***/ "./lib/olo-webui/olo-editor.js":
+/*!*************************************!*\
+  !*** ./lib/olo-webui/olo-editor.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("\n\nmodule.exports = {\n\n    template: __webpack_require__(/*! ./olo-editor.html */ \"./lib/olo-webui/olo-editor.html\"),\n\n    components: {\n        'ace-editor': __webpack_require__(/*! vue2-ace-editor */ \"./node_modules/vue2-ace-editor/index.js\"),\n    },\n    \n    props: ['value', 'lang'],\n\n    data: () => Object({\n        editorOptions: {\n            fontSize: \"12pt\",\n            //showLineNumbers: false,\n            //showGutter: false\n        }\n    }),\n    \n    computed: {\n        content: {\n            get () {\n                return this.value;\n            },\n            set (value) {\n                this.$emit('input', value);\n            }\n        }\n    },\n    \n    methods: {\n        \n        initEditor () {\n            __webpack_require__(/*! brace/ext/language_tools */ \"./node_modules/brace/ext/language_tools.js\"); //language extension prerequsite...\n            __webpack_require__(/*! brace/mode/yaml */ \"./node_modules/brace/mode/yaml.js\");\n            __webpack_require__(/*! brace/theme/chrome */ \"./node_modules/brace/theme/chrome.js\")\n        }\n    }\n}\n\n\n//# sourceURL=webpack:///./lib/olo-webui/olo-editor.js?");
+
+/***/ })
+
+}]);
