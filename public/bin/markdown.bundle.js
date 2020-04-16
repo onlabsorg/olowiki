a7b13e9f814f59e58310e7c3c538ab77a23c9977
Marcello Del Buono <m.delbuono@gmail.com>
2020-04-16 20:40:45 +0200

    created http-server and removed backend-environment

--- public/bin/markdown.bundle.js
+++ public/bin/markdown.bundle.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/bin/markdown"],{
 
-/***/ "../[ ] olojs/lib/environment/stdlib/markdown.js":
-/*!*******************************************************!*\
-  !*** ../[ ] olojs/lib/environment/stdlib/markdown.js ***!
-  \*******************************************************/
+/***/ "../[ ] olojs/lib/stdlib/markdown.js":
+/*!*******************************************!*\
+  !*** ../[ ] olojs/lib/stdlib/markdown.js ***!
+  \*******************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("const marked = __webpack_require__(/*! marked */ \"../[ ] olojs/node_modules/marked/src/marked.js\");\n\nexports.__apply__ = text => marked(text);\n\n\n//# sourceURL=webpack:///../%5B_%5D_olojs/lib/environment/stdlib/markdown.js?");
+eval("const marked = __webpack_require__(/*! marked */ \"../[ ] olojs/node_modules/marked/src/marked.js\");\n\nexports.__apply__ = text => marked(text);\n\n\n//# sourceURL=webpack:///../%5B_%5D_olojs/lib/stdlib/markdown.js?");
 
 /***/ })
 
--- public/bin/path.bundle.js
+++ public/bin/path.bundle.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/bin/path"],{
 
-/***/ "../[ ] olojs/lib/environment/stdlib/path.js":
-/*!***************************************************!*\
-  !*** ../[ ] olojs/lib/environment/stdlib/path.js ***!
-  \***************************************************/
+/***/ "../[ ] olojs/lib/stdlib/path.js":
+/*!***************************************!*\
+  !*** ../[ ] olojs/lib/stdlib/path.js ***!
+  \***************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("const Path = __webpack_require__(/*! path */ \"./node_modules/path-browserify/index.js\");\n\nmodule.exports = {\n    getBaseName: Path.basename,\n    getDirName: path => Path.dirname(path) + \"/\",\n    getExtName: Path.extname,\n    format: Path.format,\n    parse: Path.parse,\n    join: Path.join,\n    normalize: Path.normalize,\n    resolve: (...paths) => Path.resolve(\"/\", ...paths)\n};\n\n\n//# sourceURL=webpack:///../%5B_%5D_olojs/lib/environment/stdlib/path.js?");
+eval("const Path = __webpack_require__(/*! path */ \"./node_modules/path-browserify/index.js\");\n\nmodule.exports = {\n    getBaseName: Path.basename,\n    getDirName: path => Path.dirname(path) + \"/\",\n    getExtName: Path.extname,\n    format: Path.format,\n    parse: Path.parse,\n    join: Path.join,\n    normalize: Path.normalize,\n    resolve: (...paths) => Path.resolve(\"/\", ...paths)\n};\n\n\n//# sourceURL=webpack:///../%5B_%5D_olojs/lib/stdlib/path.js?");
 
 /***/ })
 
