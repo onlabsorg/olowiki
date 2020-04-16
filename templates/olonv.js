2dbdb41982cfbab81f9c617a6189d76c38cb5f90
Marcello Del Buono <m.delbuono@gmail.com>
2020-04-16 20:57:42 +0200

    updated olonv template

--- templates/olonv.js
+++ templates/olonv.js
@@ -1,6 +1,8 @@
-const Environment = olowiki.require(`backend-environment`);
+const Environment = olojs.require(`environment`);
 const FSStore = olojs.require(`stores/fs-store`);
 const Router = olojs.require('stores/router');
+const HTTPServer = olowiki.require("http-server");
+
 const EMailDispatcher = olowiki.require(`dispatchers/email`);
 // const GmailDispatcher = olowiki.require(`dispatchers/gmail`);
 
@@ -14,34 +16,38 @@ module.exports = new Environment({
         "/contributors"  : new FSStore(`${__dirname}/public/contributors`),        
     }),
 
-    // // Customize the access control.
-    // // This function accepts an (express) HTTP request object and should return
-    // // `true` to allow it adn `false` to deny it.
-    // allow: function (req) {},
-    
     nocache: true,
-    
-    owner  : "<% owner %>",
-    secret : "<% secret %>",
+        
+    globals: {
+        require: modulePath => olojs.require(`stdlib/${modulePath}`)
+    },
 
-    dispatch: EmailDispatcher({        
-        host: "...",    // e.g. smtp.mail.yahoo.com; check your e-mail provider for the host name
-        port: 465,      // ask your e-mail provider
-        secure: true,   // true for port 465, false for other ports
-        auth: {
-            user: "...",   // your e-mail user id
-            pass: "..."    // your e-mail password
-        },
+    serve: HTTPServer({
+
+        owner  : "<% owner %>",
+        secret : "<% secret %>",
+
+        dispatch: EmailDispatcher({        
+            host: "...",    // e.g. smtp.mail.yahoo.com; check your e-mail provider for the host name
+            port: 465,      // ask your e-mail provider
+            secure: true,   // true for port 465, false for other ports
+            auth: {
+                user: "...",   // your e-mail user id
+                pass: "..."    // your e-mail password
+            },
+        }),
+        
+        /** 
+         *  In case you want to use gmail to send tokens,
+         *  use GMailDispatcher = require("../lib/dispatchers/gmail") instead
+         *  and fill in the following fields; otherwise delete them.
+         */
+        // dispatch: GmailDispatcher({
+        //     from: "...",     // the sender email address
+        //     credentials: {}, // the credentials object obtained when activating the gmail API in your gmail account
+        //     token: "..."     // the token obtained after authorizing the app
+        // })
     }),
     
 
-    // // in case you want to use gmail to send tokens, set
-    // // EMailDispatcher = retuire("../lib/dispatchers/gmail") 
-    // // instead and fill in the following fields; otherwise delete them
-    // // do not forget to activate the "less secure app" option in your gmail settings
-    // dispatch: GmailDispatcher({
-    //     from: "...",     // the sender email address
-    //     credentials: {}, // the credentials object obtained when activating the gmail API in your gmail account
-    //     token: "..."     // the token obtained after authorizing the app
-    // })
 });
