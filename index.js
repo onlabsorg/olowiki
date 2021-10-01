85ae2297e5c3a2a204107cb01161e1c45c4bdc95
Marcello Del Buono <m.delbuono@gmail.com>
2021-10-01 22:24:02 +0200

    Updated plugin to stilo 0.15.0 format

--- index.js
+++ index.js
@@ -11,17 +11,12 @@ const Server = exports.Server = require('./lib/server');
 // STILO PLUGIN
 exports.stilo = {
     
-    routes: {},
-    
-    commands: {
-        
-        "wiki": async (store, options={}) => {
-            const server = Server(store.homeStore);
-            const port = options.port || 8010;
-            server.listen(port, err => {
-                if (err) throw err;
-                console.log(`oloWiki server listening on port ${port}`);
-            });
-        }
+    async wiki (store, options={}) {
+        const server = Server(store.homeStore);
+        const port = options.port || 8010;
+        server.listen(port, err => {
+            if (err) throw err;
+            console.log(`oloWiki server listening on port ${port}`);
+        });
     }
 }
